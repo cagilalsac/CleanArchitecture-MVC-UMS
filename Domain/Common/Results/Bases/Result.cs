895f325b2e89710def7804e7483c94a0d68811c6
Çağıl Alsaç <cagilalsac@outlook.com>
2024-03-27 21:25:52 +0300

    vFinal

--- Domain/Common/Results/Bases/Result.cs
+++ Domain/Common/Results/Bases/Result.cs
@@ -1,11 +1,11 @@
 ﻿namespace Domain.Common.Results.Bases
 {
-	public class Result
+	public abstract class Result
 	{
 		public bool IsSuccessful { get; }
         public string Message { get; }
 
-		public Result(bool isSuccessful, string message)
+		protected Result(bool isSuccessful, string message)
 		{
 			IsSuccessful = isSuccessful;
 			Message = message;
